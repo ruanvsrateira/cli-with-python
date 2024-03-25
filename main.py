1700639a09ff780195ee99d74cae190d363eb050
ruanvsrateira <vruan85@gmail.com>
2024-03-25 17:37:08 -0300

    feat: create custom messages with pyfiglet

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import pyfiglet
 from time import sleep
 from subprocess import Popen
 
+exit_commands = ["quit", "exit", "bye", "goodbye"]
+
+print(pyfiglet.figlet_format("Python Custom CLI"))
+
 while True:
   command = input("\n$(root) ")
+  
+  if command in exit_commands:
+    print(pyfiglet.figlet_format("Good Bye."))
+    break
+  
   with Popen(command, shell=True) as pop:
-    sleep(0.5)   
+    sleep(0.3)   
     continue
 
